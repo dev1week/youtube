89165d1f486d4e8c7ef6df91b6ac6d71f23ea9f1
hanju <the_1_week@naver.com>
2023-08-30 12:14:53 +0900

    #2 -3 youtube list 조회
    1. axiosInstance  생성 -> baseUrl 등록
    2. axiosInstance.get메서드 활용 결과 받아오기
    3. useState 활용 받아온 데이터 저장

--- src/useYoutubeData.ts
+++ src/useYoutubeData.ts
@@ -1,13 +1,116 @@
 import { useCallback, useState } from "react";
 import { TypeListItem } from "./TypeListItem";
+import axios from "axios";
+import ApiKey  from "./API_KEY";
+
+
+const API_KEY ='';
+const axiosInstance = axios.create({
+    baseURL:'https://www.googleapis.com/youtube/v3',
+})
+
+type TypeVideoListResponse = {
+    kind: 'youtube#videoListResponse';
+    etag: string;
+    nextPageToken: string;
+    prevPageToken: string;
+    pageInfo: {
+      totalResults: number;
+      resultsPerPage: number;
+    };
+    items: {
+      kind: 'youtube#video';
+      etag: string;
+      id: string;
+      snippet: {
+        publishedAt: string;
+        channelId: string;
+        title: string;
+        description: string;
+        thumbnails: {
+          [key: string]: {
+            url: string;
+            width: number;
+            height: number;
+          };
+        };
+        channelTitle: string;
+        tags: string[];
+        categoryId: string;
+      };
+      contentDetails: {
+        duration: string;
+        dimension: string;
+        definition: string;
+        caption: string;
+        licensedContent: boolean;
+        regionRestriction: {
+          allowed: [string];
+          blocked: [string];
+        };
+        contentRating: {
+          mpaaRating: string;
+          tvpgRating: string;
+          bbfcRating: string;
+          chvrsRating: string;
+          eirinRating: string;
+          cbfcRating: string;
+          fmocRating: string;
+          icaaRating: string;
+          acbRating: string;
+          oflcRating: string;
+          fskRating: string;
+          kmrbRating: string;
+          djctqRating: string;
+          russiaRating: string;
+          rtcRating: string;
+          ytRating: string;
+        };
+      };
+      statistics: {
+        viewCount: number;
+        likeCount: number;
+        dislikeCount: number;
+        favoriteCount: number;
+        commentCount: number;
+      };
+    }[];
+  };
+
 
 
 export const useYoutubeData = () =>{
-    const [data] = useState<TypeListItem[]> ([]);
+    const [data, setData] = useState<TypeListItem[]> ([]);
 
     const loadData= useCallback(async () =>{
-
-    }, []);
+       
+        try{
+            const videoResults = await axiosInstance.get<TypeVideoListResponse>(
+                '/videos',{
+                    params:{
+                        key: API_KEY,
+                        part: 'snippet, contentDetails, statistics',
+                        chart: 'mostPopular',
+                        regionCode: 'KR',
+                    }
+                }
+            )
+            const videoData = videoResults.data;
+            setData(
+                videoData.items.map(item=>(
+                    {
+                        title:item.snippet.title, 
+                        thumbnail:item.snippet.thumbnails.high.url, 
+                        publishedAt:item.snippet.publishedAt, 
+                        viewCount:item.statistics.viewCount,
+                        channelTitle:item.snippet.channelTitle
+                     })),
+                );
+        }catch(ex){
+            console.log(ex);
+        }
+    },[]);
+    
 
     return {
         data, 
